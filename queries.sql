23ce152763e42bb1de1c68b7cede3a4065d73fbe
Marco <marco_mireles@outlook.com>
2022-10-26 23:43:41 -0500

    made a new table dept_info

--- queries.sql
+++ queries.sql
@@ -145,3 +145,14 @@ FROM dept_manager AS dm
         ON (dm.dept_no = d.dept_no)
     INNER JOIN current_emp AS ce
         ON (dm.emp_no = ce.emp_no);
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
