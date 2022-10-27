6910fdbc4ee7f96c025910cae54899fc2647cdce
Marco <marco_mireles@outlook.com>
2022-10-26 19:56:15 -0500

    joined tables

--- queries.sql
+++ queries.sql
@@ -49,3 +49,12 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Check the table
 SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
