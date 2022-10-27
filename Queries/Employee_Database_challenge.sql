281967579ee8dcab5b4b8a5c016272174bf83f58
Marco <marco_mireles@outlook.com>
2022-10-27 01:11:41 -0500

    created unique_titles

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -10,3 +10,14 @@ WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
 SELECT * FROM retirement_titles;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title,
+	to_date
+INTO unique_titles 
+FROM retirement_titles 
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
