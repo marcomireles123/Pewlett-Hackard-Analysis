35eca471f6bb4eba6693081867ab044873668099
Marco <marco_mireles@outlook.com>
2022-10-27 00:02:02 -0500

    made a table for sales and developers

--- queries.sql
+++ queries.sql
@@ -157,7 +157,7 @@ ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
--- Skill drill 7.3.6
+-- Skill drill #1 7.3.6
 SELECT ce.emp_no,
 	ce.first_name,
 	ce.last_name,
@@ -169,3 +169,17 @@ ON (ce.emp_no = de.emp_no)
 INNER JOIN departments as d
 ON (de.dept_no = d.dept_no)
 WHERE (d.dept_name = 'Sales');
+
+-- Skill drill #2 7.3.6
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info_sales_devlp
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name in ('Sales','Development')
+ORDER BY d.dept_name DESC;
