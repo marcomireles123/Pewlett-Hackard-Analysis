5bd4243f270d96ece3a72608180b6214893e7dfd
Marco <marco_mireles@outlook.com>
2022-10-26 23:59:40 -0500

    added a table for dept_info_sales

--- queries.sql
+++ queries.sql
@@ -156,3 +156,16 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+-- Skill drill 7.3.6
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info_sales
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
